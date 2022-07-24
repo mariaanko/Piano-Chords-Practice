a986527e52132298529bacbbfd973282ccbf87d8
mariaanko <vaclavik.marian@gmail.com>
2022-07-24 11:47:15 +0200

    there were duplicate images ffs - now fixed

--- app/src/main/java/com/wodpress/mariaanko/pianochordspractice/ChordsAdapter.java
+++ app/src/main/java/com/wodpress/mariaanko/pianochordspractice/ChordsAdapter.java
@@ -86,25 +86,4 @@ public class ChordsAdapter extends RecyclerView.Adapter<ChordsAdapter.MyViewHold
     public interface OnChordListener{
         void onChordClick(int position, View view);
     }
-
-    public static class ItemOffsetDecoration extends RecyclerView.ItemDecoration {
-
-        private int mItemOffset;
-
-        public ItemOffsetDecoration(int itemOffset) {
-            mItemOffset = itemOffset;
-        }
-
-        public ItemOffsetDecoration(@NonNull Context context, @DimenRes int itemOffsetId) {
-            this(context.getResources().getDimensionPixelSize(itemOffsetId));
-        }
-
-        @Override
-        public void getItemOffsets(Rect outRect, View view, RecyclerView parent,
-                                   RecyclerView.State state) {
-            super.getItemOffsets(outRect, view, parent, state);
-            outRect.set(mItemOffset, mItemOffset, mItemOffset, mItemOffset);
-        }
-
-    }
 }
